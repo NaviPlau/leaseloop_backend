17fba93bf4c006c528cd47d56874842c9457782a
Benjamin Tietz <benjamin0tietz@gmail.com>
2025-04-06 20:58:53 +0200

    fix: typo in promocode model

--- promocodes/models.py
+++ promocodes/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 class Promocodes(models.Model):
     code = models.CharField(max_length=20)
     description = models.CharField(max_length=255)
-    valid_unitil = models.DateField()
+    valid_until = models.DateField()
     discount_percent = models.DecimalField(max_digits=5, decimal_places=2)
     owner_id = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='promocodes'
