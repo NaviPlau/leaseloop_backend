c8666b125f41d78bf764640f02d0a951e52e1211
NaviPlau <naviplau@outlook.com>
2025-04-06 22:22:38 +0200

    chore: delete image view

--- properties/views.py
+++ properties/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status, permissions
 from django.shortcuts import get_object_or_404
 from rest_framework.permissions import AllowAny
-from .models import Property
+from .models import Property , PropertyImage
 from .serializers import PropertySerializer
 from rest_framework.parsers import MultiPartParser, FormParser
 from .serializers import PropertyImageSerializer
@@ -97,3 +97,13 @@ class PropertyImageUploadView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        image = get_object_or_404(PropertyImage, pk=pk)
+
+        # Optional: check ownership
+        if image.property.owner != request.user:
+            return Response({'error': 'Not authorized to delete this image.'}, status=status.HTTP_403_FORBIDDEN)
+
+        image.delete()
+        return Response({'message': 'Image deleted successfully.'}, status=status.HTTP_204_NO_CONTENT)
