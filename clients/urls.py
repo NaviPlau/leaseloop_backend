2ff3b88af1317bd944a6b760bc63e64bd88e8f91
Benjamin Tietz <benjamin0tietz@gmail.com>
2025-04-10 18:38:39 +0200

    fix: typo in clients url

--- clients/urls.py
+++ clients/urls.py
@@ -3,5 +3,5 @@ from .views import ClientAPIView
 
 urlpatterns = [
     path('clients/', ClientAPIView.as_view()),
-    path('clients/<int:pk>/', ClientAPIView.as_view()),
+    path('client/<int:pk>/', ClientAPIView.as_view()),
 ]
