204245fb65a13eaf3f8b8aab059746a9b18253a5
Benjamin Tietz <benjamin0tietz@gmail.com>
2025-04-15 14:17:08 +0200

    feat(admin): add admin interface for Service model

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Service
 
-# Register your models here.
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'type', 'price', 'property', 'created_at', 'updated_at')
+    list_filter = ('type', 'property', 'created_at')
+    search_fields = ('name', 'property__name')
+    ordering = ('property', 'name')
