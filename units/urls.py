263f8c2ef656b73fbe50f430f7aeca3a2f92fdcc
Benjamin Tietz <benjamin0tietz@gmail.com>
2025-06-15 17:38:39 +0200

    refactor: unit app endpoint fix typo

--- units/urls.py
+++ units/urls.py
@@ -3,7 +3,7 @@ from .views import UnitAPIView, UnitImageView, AmenityListAPIView
 
 urlpatterns = [
     path('units/', UnitAPIView.as_view()),
-    path('units/<int:pk>/', UnitAPIView.as_view()),
+    path('unit/<int:pk>/', UnitAPIView.as_view()),
     path('properties/<int:property_id>/units/', UnitAPIView.as_view()),
     path('unit-images/', UnitImageView.as_view(), name='unit-image-upload'),
     path('unit-image/<int:pk>/', UnitImageView.as_view(), name='unit-image-delete'),
