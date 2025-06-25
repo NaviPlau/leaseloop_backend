91651daacc8e50f2696f3fc3db880f1285a675b7
Benjamin Tietz <benjamin0tietz@gmail.com>
2025-06-25 20:22:46 +0200

    docs: format code and add docstrings in units app

--- units/serializers.py
+++ units/serializers.py
@@ -27,6 +27,17 @@ class UnitSerializer(serializers.ModelSerializer):
         extra_fields = ['amenities_ids']
 
     def create(self, validated_data):
+        """
+        Creates a new unit instance.
+
+        This method takes validated data, creates a new unit instance from it, and
+        then assigns the provided property and amenities to the unit.
+
+        :param validated_data: A dictionary containing the validated data for
+        creating a new unit. It should include keys like 'name', 'description',
+        'price_per_night', 'max_capacity', 'property_id', 'amenities'.
+        :return: The newly created unit instance.
+        """
         property_obj = validated_data.pop('property_id')
         amenities_data = validated_data.pop('amenities', [])
         unit = Unit.objects.create(property=property_obj, **validated_data)
@@ -34,6 +45,19 @@ class UnitSerializer(serializers.ModelSerializer):
         return unit
     
     def update(self, instance, validated_data):
+        """
+        Updates an existing unit instance with validated data.
+
+        This method takes the validated data, updates the unit instance with the
+        provided data, and then assigns the provided amenities to the unit.
+
+        :param instance: The existing unit instance to be updated.
+        :param validated_data: A dictionary containing the validated data for
+        updating the unit. It may include keys like 'name', 'description',
+        'price_per_night', 'max_capacity', 'amenities', 'active', 'type', and
+        'status'.
+        :return: The updated unit instance.
+        """
         amenities_data = validated_data.pop('amenities', None)
         instance.name = validated_data.get('name', instance.name)
         instance.description = validated_data.get('description', instance.description)
@@ -50,6 +74,17 @@ class UnitSerializer(serializers.ModelSerializer):
         return instance
     
     def get_image_url(self, obj):
+        """
+        Returns the URL of the first image of the unit if it exists, otherwise returns None.
+
+        This method is used to build the URL of the first image of the unit based on the
+        request object. If the request object is not None, it appends the request's base URL
+        to the image's relative URL. If the request object is None, it returns the image's
+        relative URL as is.
+
+        :param obj: The unit object for which the image URL is being generated.
+        :return: The URL of the first image of the unit if it exists, otherwise None.
+        """
         first_image = obj.images.first()
         if first_image and first_image.image:
             request = self.context.get('request')
