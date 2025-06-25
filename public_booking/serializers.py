5eb217ae5901e665f1a48be1390dc3a8bddc5c9d
Benjamin Tietz <benjamin0tietz@gmail.com>
2025-06-25 20:18:26 +0200

    docs: format code and add docstrings in public_booking app

--- public_booking/serializers.py
+++ public_booking/serializers.py
@@ -16,12 +16,21 @@ class ServiceSerializer(serializers.ModelSerializer):
         model = Service
         fields = '__all__'
 
-
 class PublicOwnerBookingPageSerializer(serializers.Serializer):
     owners = serializers.SerializerMethodField()
     properties = serializers.SerializerMethodField()
 
     def get_owners(self, obj):
+        """
+        Returns a list of dictionaries containing the owner id, name and slug of
+        every owner in the obj['owners'] list.
+
+        Args:
+            obj (dict): A dictionary with the key 'owners' containing a list of owners
+
+        Returns:
+            list: A list of dictionaries containing the owner id, name and slug
+        """
         return [
             {
                 'id': owner.id,
@@ -32,6 +41,17 @@ class PublicOwnerBookingPageSerializer(serializers.Serializer):
         ]
 
     def get_properties(self, obj):
+        """
+        Returns a list of PropertySerializer objects for every property in the
+        obj['properties'] list.
+
+        Args:
+            obj (dict): A dictionary with the key 'properties' containing a list of
+            properties
+
+        Returns:
+            list: A list of PropertySerializer objects
+        """
         return PropertySerializer(obj['properties'], many=True, context=self.context).data
 
 
