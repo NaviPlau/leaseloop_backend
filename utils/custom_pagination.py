d2fc775f3619f1e31a1858cf3cccafef4a58d3c6
Benjamin Tietz <benjamin0tietz@gmail.com>
2025-06-25 20:24:41 +0200

    docs: format code and add docstrings in utils app

--- utils/custom_pagination.py
+++ utils/custom_pagination.py
@@ -8,6 +8,17 @@ class CustomPageNumberPagination(PageNumberPagination):
     page_size_query_param = 'page_size'
 
     def get_paginated_response(self, data):
+        """
+        Returns a response object with paginated data.
+
+        :param data: The serialized page objects
+        :return: A response object with the following keys:
+            - count: The total number of objects
+            - total_pages: The total number of pages
+            - next: The URL of the next page, or None
+            - previous: The URL of the previous page, or None
+            - results: The serialized page objects
+        """
         total_pages = math.ceil(self.page.paginator.count / self.page_size)
 
         return Response({
